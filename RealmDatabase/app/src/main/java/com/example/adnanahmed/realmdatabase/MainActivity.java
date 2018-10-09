3fd861011cb92177df759ea1a80667a4f97d35ef
adnan94 <adnan.ahmed94k@gmail.com>
2018-10-09 06:27:29 -0700

    commit

--- RealmDatabase/app/src/main/java/com/example/adnanahmed/realmdatabase/MainActivity.java
+++ RealmDatabase/app/src/main/java/com/example/adnanahmed/realmdatabase/MainActivity.java
@@ -2,6 +2,19 @@ package com.example.adnanahmed.realmdatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.internal.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +23,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Realm realm = Realm.getDefaultInstance(); // opens "myrealm.realm"
 
-        }
+
+
+        RealmConfiguration config = new RealmConfiguration.Builder()
+                .deleteRealmIfMigrationNeeded()
+                .build();
+        realm.setDefaultConfiguration(config);
+        ((Button) findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final User user = new User();
+                user.setName("Adnan Ahmed");
+                user.setCity("Karachi");
+                user.setCountry("Pakistan");
+                user.setId("adnan_ahmed1");
+                realm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        realm.copyToRealmOrUpdate(user);
+                    }
+                });
+            }
+        });
+
+    }
+
+
 }
---
+++ RealmDatabase/app/src/main/java/com/example/adnanahmed/realmdatabase/User.java
@@ -0,0 +1,43 @@
+package com.example.adnanahmed.realmdatabase;
+
+import io.realm.RealmObject;
+
+public class User extends RealmObject{
+    String name;
+    String city;
+    String country;
+    String id;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
