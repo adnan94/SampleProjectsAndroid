95964a4947854fa557bb6aa25db932bd55d313be
adnan94 <adnan.ahmed94k@gmail.com>
2019-08-05 20:31:23 +0500

    commit

---
+++ DemoHorizontalUi/app/src/main/java/com/revolutionary/demohorizontalui/MainActivity.java
@@ -0,0 +1,23 @@
+package com.revolutionary.demohorizontalui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//        if you want only landscape
+//        if (!getResources().getBoolean(R.bool.portrait_only)) {
+//            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+//        } else {
+//            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+//
+//        }
+
+    }
+}
