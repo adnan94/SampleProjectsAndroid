531ce905d2d42373682db51c7aac00bb47d3af57
adnan94 <adnan.ahmed94k@gmail.com>
2019-08-09 18:43:52 +0500

    commit

--- DemoHorizontalUi/app/src/main/java/com/revolutionary/demohorizontalui/MainActivity.java
+++ DemoHorizontalUi/app/src/main/java/com/revolutionary/demohorizontalui/MainActivity.java
@@ -2,6 +2,7 @@ package com.revolutionary.demohorizontalui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Adnan", "Create");
 
 
 //        if you want only landscape
@@ -19,5 +21,42 @@ public class MainActivity extends AppCompatActivity {
 //
 //        }
 
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        Log.d("Adnan", "Destroy");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("Adnan", "Resume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("Adnan", "Start");
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("Adnan", "Stop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("Adnan", "Restart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("Adnan","Pause");
+        super.onPause();
     }
 }
